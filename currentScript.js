e9e6b054fc56c9a2545522aa9896aaf32ae26579
Jhay-Great <alemonai.jenef@gmail.com>
2023-10-27 13:26:23 +0000

    refracted btn1To20

--- currentScript.js
+++ currentScript.js
@@ -5,16 +5,16 @@ const btn1N50 = document.getElementById("oneToFifty");
 const btn1N100 = document.getElementById("oneToHundred");
 const btnCheck = document.querySelector(".check");
 const btnPlayAgain = document.querySelector(".play-again");
-const btnPAgain = document.querySelector('.pAgain');
+const btnPAgain = document.querySelector(".pAgain");
 
-const popUp = document.querySelector('.pop-up');
-const popUpOverlay = document.querySelector('.blur');
-const popUpCloseBtn = document.querySelector('.close-btn');
-const gameInfoBtn = document.querySelector('.game-info');
+const popUp = document.querySelector(".pop-up");
+const popUpOverlay = document.querySelector(".blur");
+const popUpCloseBtn = document.querySelector(".close-btn");
+const gameInfoBtn = document.querySelector(".game-info");
 
 //text variables
 const subHeading = document.querySelector("h2");
-const hiddenNumber = document.querySelector('.secret-number');
+const hiddenNumber = document.querySelector(".secret-number");
 const outputMessage = document.querySelector(".message");
 const scoreValue = document.querySelector(".score");
 const highScoreValue = document.querySelector(".highscore");
@@ -28,14 +28,14 @@ let lastRangeValue;
 //simple functions - On buttons
 const inputFieldOn = function () {
   inputBox.disabled = false;
-  inputBox.value = '';
+  inputBox.value = "";
 };
 const btnCheckOn = function () {
   btnCheck.disabled = false;
 };
 const btnPlayAgainOn = function () {
-    btnPlayAgain.disabled = false;
-}
+  btnPlayAgain.disabled = false;
+};
 
 //Off buttons
 const inputFieldOff = function () {
@@ -45,8 +45,8 @@ const btnCheckOff = function () {
   btnCheck.disabled = true;
 };
 const btnPlayAgainOff = function () {
-    btnPlayAgain.disabled = true;
-}
+  btnPlayAgain.disabled = true;
+};
 
 //text functionality
 const outputMsgDisplay = function (text) {
@@ -55,365 +55,244 @@ const outputMsgDisplay = function (text) {
 
 //game info and blur overlay functionality
 let showModal = function () {
-    popUp.classList.remove('hidden');
-    popUpOverlay.classList.remove('hidden');
-}
+  popUp.classList.remove("hidden");
+  popUpOverlay.classList.remove("hidden");
+};
 const closeModal = function () {
-    popUp.classList.add('hidden');
-    popUpOverlay.classList.add('hidden');
-}
+  popUp.classList.add("hidden");
+  popUpOverlay.classList.add("hidden");
+};
 
+// Generating random number
+const getRandomNumber = function (number) {
+  return Math.trunc(Math.random() * number) + 1;
+};
+
+const playAgainFn = function() {
+  randomDigits = getRandomNumber(lastRangeValue);
+    hiddenNumber.textContent = "?";
+
+    outputMsgDisplay("Start guessing...");
+
+    scoreNum = lastRangeValue; 
+    scoreValue.textContent = scoreNum;
+
+    hiddenNumber.style.boxShadow = "initial";
+
+    inputFieldOn();
+    btnCheckOn();
+}
 
 inputFieldOff();
 btnCheckOff();
 btnPlayAgainOff();
 showModal();
 
-
-
 // game Decision
-const compareResult = function() {
- 
-    let guessedNumber = Number(document.querySelector(".guesses").value);
+const compareResult = function () {
+  let guessedNumber = Number(inputBox.value);
 
-    //when no guess or guess is out of range
-    if (!guessedNumber || guessedNumber < 0 || guessedNumber > lastRangeValue) {
-      outputMsgDisplay(`💥 Enter a number between 1 and ${lastRangeValue}`);
-      scoreNum--;
+  //when no guess or guess is out of range
+  if (!guessedNumber || guessedNumber < 0 || guessedNumber > lastRangeValue) {
+    outputMsgDisplay(`💥 Enter a number between 1 and ${lastRangeValue}`);
+    scoreNum--;
+    scoreValue.textContent = scoreNum;
+    if (scoreNum < 1) {
+      outputMsgDisplay("😞🙄 Game over");
+      scoreNum = 0;
       scoreValue.textContent = scoreNum;
-      if (scoreNum < 1) {
-        outputMsgDisplay('😞🙄 Game over')
-        scoreNum = 0;
-        scoreValue.textContent = scoreNum;
-        btnCheckOff();
-        inputFieldOff();
-        hiddenNumber.style.boxShadow = '5px 3px 5px red';
-      }
-    } //when guess is correct
-    else if (guessedNumber === randomDigits) {
-      outputMsgDisplay("🍾 Yayy!!! Correct number!");
       btnCheckOff();
       inputFieldOff();
-      hiddenNumber.textContent = randomDigits;
-      if (highscoreNum < scoreNum) {
-        highscoreNum = scoreNum;
-        highScoreValue.textContent = highscoreNum;
-      }
-      hiddenNumber.style.boxShadow = '5px 3px 5px #07f72b';
-    } //when guess is wrong 
-    else if (guessedNumber !== randomDigits) {
-      outputMsgDisplay(
-        guessedNumber > randomDigits ? "📈 Too high" : "📉 Too low"
-      );
-      scoreNum--;
-      scoreValue.textContent = scoreNum;
-      if (scoreNum < 1) {
-        outputMsgDisplay('😞 Game over')
-        scoreNum = 0;
-        scoreValue.textContent = scoreNum;
-        btnCheckOff();
-        inputFieldOff();
-        hiddenNumber.style.boxShadow = '5px 3px 5px red';
-      }
+      hiddenNumber.style.boxShadow = "5px 3px 5px red";
     }
-   
-}
-
+  } //when guess is correct
+  else if (guessedNumber === randomDigits) {
+    outputMsgDisplay("🍾 Yayy!!! Correct number!");
+    btnCheckOff();
+    inputFieldOff();
+    hiddenNumber.textContent = randomDigits;
+    if (highscoreNum < scoreNum) {
+      highscoreNum = scoreNum;
+      highScoreValue.textContent = highscoreNum;
+    }
+    hiddenNumber.style.boxShadow = "5px 3px 5px #07f72b";
+  } //when guess is wrong
+  else if (guessedNumber !== randomDigits) {
+    outputMsgDisplay(
+      guessedNumber > randomDigits ? "📈 Too high" : "📉 Too low"
+    );
+    scoreNum--;
+    scoreValue.textContent = scoreNum;
+    if (scoreNum < 1) {
+      outputMsgDisplay("😞 Game over");
+      scoreNum = 0;
+      scoreValue.textContent = scoreNum;
+      btnCheckOff();
+      inputFieldOff();
+      hiddenNumber.style.boxShadow = "5px 3px 5px red";
+    }
+  }
+};
 
 // /**--------------------------------------------Event functionality------------------------------------------------ */
 //                               /*---------------------1 and 20---------------*/
 
-btn1N20.addEventListener('click', function() {
+btn1N20.addEventListener("click", function () {
   lastRangeValue = 20;
   subHeading.textContent = `between 1 and ${lastRangeValue}`;
-  hiddenNumber.textContent = '?';
-  hiddenNumber.style.boxShadow = 'initial';
-  outputMsgDisplay('Start guessing...');
-
+  hiddenNumber.textContent = "?";
+  hiddenNumber.style.boxShadow = "initial";
+  outputMsgDisplay("Start guessing...");
 
   inputFieldOn();
   btnCheckOn();
   btnPlayAgainOn();
 
-  randomDigits = Math.trunc(Math.random() * 20) + 1;  //try put this in a function and set the multiplier to lastRangeNumber
-  scoreNum = 20;
+  randomDigits = getRandomNumber(lastRangeValue);
+  scoreNum = lastRangeValue;
   scoreValue.textContent = scoreNum;
   highscoreNum = 0;
   highScoreValue.textContent = 0;
 
-  btnCheck.addEventListener('click', compareResult);
+  btnCheck.addEventListener("click", compareResult);
 
-  
-  //Play again functionality
-  btnPlayAgain.addEventListener('click', function() {
-    randomDigits = Math.trunc(Math.random() * 20) + 1;
-    hiddenNumber.textContent = '?';
+  // play again functionality for desktop version
+  btnPlayAgain.addEventListener("click", playAgainFn);
 
-    outputMsgDisplay('Start guessing...');
-
-    scoreNum = 20;   //further examination
-    scoreValue.textContent = scoreNum;
-
-    hiddenNumber.style.boxShadow = 'initial';
-
-    inputFieldOn();
-    btnCheckOn();
-
-  })
-
-  //Play again functionality for small screens - refractor later
-  btnPAgain.addEventListener('click', function() {
-    randomDigits = Math.trunc(Math.random() * 20) + 1;
-    hiddenNumber.textContent = '?';
-
-    outputMsgDisplay('Start guessing...');
-
-    scoreNum = 20;   //further examination
-    scoreValue.textContent = scoreNum;
-
-    hiddenNumber.style.boxShadow = 'initial';
-
-    inputFieldOn();
-    btnCheckOn();
-
-  })
+  //Play again functionality for small screens
+  btnPAgain.addEventListener("click", playAgainFn);
 })
-// btnPlayAgain.addEventListener('click', playAgain);
 
+/*---------------------1 and 50---------------*/
 
-                               /*---------------------1 and 50---------------*/
-
-btn1N50.addEventListener('click', function() {
+btn1N50.addEventListener("click", function () {
   lastRangeValue = 50;
   subHeading.textContent = `between 1 and ${lastRangeValue}`;
-  hiddenNumber.textContent = '?';
-  hiddenNumber.style.boxShadow = 'initial';
-  outputMsgDisplay('Start guessing...');
-
+  hiddenNumber.textContent = "?";
+  hiddenNumber.style.boxShadow = "initial";
+  outputMsgDisplay("Start guessing...");
 
   inputFieldOn();
   btnCheckOn();
   btnPlayAgainOn();
 
-  randomDigits = Math.trunc(Math.random() * 50) + 1;
+  randomDigits = getRandomNumber(lastRangeValue);
   scoreNum = 15;
   scoreValue.textContent = scoreNum;
   highscoreNum = 0;
   highScoreValue.textContent = 0;
 
-  btnCheck.addEventListener('click', compareResult)
+  btnCheck.addEventListener("click", compareResult);
 
-  
   //Play again functionality
-  btnPlayAgain.addEventListener('click', function () {
-    randomDigits = Math.trunc(Math.random() * 50) + 1;
-    hiddenNumber.textContent = '?';
+  btnPlayAgain.addEventListener("click", function () {
+    randomDigits = getRandomNumber(lastRangeValue);
+    hiddenNumber.textContent = "?";
 
-    outputMsgDisplay('Start guessing...');
+    outputMsgDisplay("Start guessing...");
 
-    
-    scoreNum = 15;   //further examination
+    scoreNum = 15; //further examination
     scoreValue.textContent = scoreNum;
 
-    hiddenNumber.style.boxShadow = 'initial';
+    hiddenNumber.style.boxShadow = "initial";
 
     inputFieldOn();
     btnCheckOn();
-
-  })
+  });
 
   //Play again functionality for small screens - refractor later
-  btnPAgain.addEventListener('click', function() {
-    randomDigits = Math.trunc(Math.random() * 50) + 1;
-    hiddenNumber.textContent = '?';
+  btnPAgain.addEventListener("click", function () {
+    randomDigits = getRandomNumber(lastRangeValue);
+    hiddenNumber.textContent = "?";
 
-    outputMsgDisplay('Start guessing...');
+    outputMsgDisplay("Start guessing...");
 
-    scoreNum = 15;   //further examination
+    scoreNum = 15; //further examination
     scoreValue.textContent = scoreNum;
 
-    hiddenNumber.style.boxShadow = 'initial';
+    hiddenNumber.style.boxShadow = "initial";
 
     inputFieldOn();
     btnCheckOn();
-
-  })
-})
-
+  });
+});
 
 //                               /*---------------------1 and 100---------------*/
 
-btn1N100.addEventListener('click', function() {
+btn1N100.addEventListener("click", function () {
   lastRangeValue = 100;
   subHeading.textContent = `between 1 and ${lastRangeValue}`;
-  hiddenNumber.textContent = '?';
-  hiddenNumber.style.boxShadow = 'initial';
-  outputMsgDisplay('Start guessing...');
+  hiddenNumber.textContent = "?";
+  hiddenNumber.style.boxShadow = "initial";
+  outputMsgDisplay("Start guessing...");
 
   inputFieldOn();
   btnCheckOn();
   btnPlayAgainOn();
 
-  randomDigits = Math.trunc(Math.random() * 100) + 1;  //try put this in a function and set the multiplier to lastRangeNumber
+  randomDigits = Math.trunc(Math.random() * 100) + 1; //try put this in a function and set the multiplier to lastRangeNumber
   scoreNum = 10;
   scoreValue.textContent = scoreNum;
   highscoreNum = 0;
   highScoreValue.textContent = 0;
 
-  btnCheck.addEventListener('click', compareResult);
+  btnCheck.addEventListener("click", compareResult);
 
-  
   //Play again functionality - refractor later
-  btnPlayAgain.addEventListener('click', function() {
+  btnPlayAgain.addEventListener("click", function () {
     randomDigits = Math.trunc(Math.random() * 100) + 1;
-    hiddenNumber.textContent = '?';
+    hiddenNumber.textContent = "?";
 
-    outputMsgDisplay('Start guessing...');
+    outputMsgDisplay("Start guessing...");
 
-    scoreNum = 10;   //further examination
+    scoreNum = 10; //further examination
     scoreValue.textContent = scoreNum;
 
-    hiddenNumber.style.boxShadow = 'initial';
+    hiddenNumber.style.boxShadow = "initial";
 
     inputFieldOn();
     btnCheckOn();
-  })
+  });
 
   //Play again functionality for small screens - refractor later
-  btnPAgain.addEventListener('click', function() {
+  btnPAgain.addEventListener("click", function () {
     randomDigits = Math.trunc(Math.random() * 100) + 1;
-    hiddenNumber.textContent = '?';
+    hiddenNumber.textContent = "?";
 
-    outputMsgDisplay('Start guessing...');
+    outputMsgDisplay("Start guessing...");
 
-    scoreNum = 10;   //further examination
+    scoreNum = 10; //further examination
     scoreValue.textContent = scoreNum;
 
-    hiddenNumber.style.boxShadow = 'initial';
+    hiddenNumber.style.boxShadow = "initial";
 
     inputFieldOn();
     btnCheckOn();
+  });
+});
 
-  })
-})
+gameInfoBtn.addEventListener("click", showModal);
+popUpCloseBtn.addEventListener("click", closeModal);
+popUpOverlay.addEventListener("click", closeModal);
 
-gameInfoBtn.addEventListener('click', showModal)
-popUpCloseBtn.addEventListener('click', closeModal)
-popUpOverlay.addEventListener('click', closeModal)
+const dblTap = document.querySelector(".dbl-tap");
+const jhgInitials = document.getElementById("JhG");
 
-
-const dblTap = document.querySelector('.dbl-tap');
-const jhgInitials = document.getElementById('JhG');
-
-
-/** slider functionality 
+/** slider functionality
  * will have to improve this.
-*/
-const rangeMenu = document.querySelector('.dropdown-container');
-
-const showAside = function() {
-    rangeMenu.classList.remove('slider');
-    jhgInitials.style.zIndex = '2';
-    
-  }
-  const closeAside = function() {
-    rangeMenu.classList.add('slider');
-}
-
-dblTap.addEventListener('dblclick', showAside);
-window.addEventListener('click', closeAside);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ */
+const rangeMenu = document.querySelector(".dropdown-container");
+
+const showAside = function () {
+  rangeMenu.classList.remove("slider");
+  jhgInitials.style.zIndex = "2";
+};
+const closeAside = function () {
+  rangeMenu.classList.add("slider");
+};
+
+dblTap.addEventListener("dblclick", showAside);
+window.addEventListener("click", closeAside);
 
 // const rangeB = document.querySelectorAll('.between');
 
@@ -427,26 +306,7 @@ window.addEventListener('click', closeAside);
 
 // rangeB.addEventListener('click', range);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /**-------------------------------------------THE END----------------------------------------------------------------------- */
-
-
 
 // /**--------------------------------------------Event functionality------------------------------------------------ */
 //                               /*---------------------1 and 20---------------*/
@@ -496,7 +356,7 @@ window.addEventListener('click', closeAside);
 //         highscoreNum = scoreNum;
 //         highScoreValue.textContent = highscoreNum;
 //       }
-//     } //when guess is wrong 
+//     } //when guess is wrong
 //     else if (guessedNumber !== randomDigits) {
 //       outputMsgDisplay(
 //         guessedNumber > randomDigits ? "📈 Too high" : "📉 Too low"
@@ -528,7 +388,6 @@ window.addEventListener('click', closeAside);
 //   });
 // });
 
-
 //                               /*---------------------1 and 50---------------*/
 // btn1N50.addEventListener('click', function() {
 //   subHeading.textContent = "between 1 and 50";
@@ -556,8 +415,4 @@ window.addEventListener('click', closeAside);
 //   console.log(scoreNum);
 // })
 
-
-
-                              /*---------------------1 and 100---------------*/
-
-
+/*---------------------1 and 100---------------*/
